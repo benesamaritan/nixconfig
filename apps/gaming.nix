2a4ebe812e9b4b206c88d5c4986403fee5ed58f1
benesamaritan <samaritan@autistici.org>
2025-12-26 12:39:28 +0700

    update comment

--- apps/gaming.nix
+++ apps/gaming.nix
@@ -11,7 +11,7 @@
     enable = true;
     settings = {
       general = {
-        renice = 10;  # Lower niceness for game processes
+        renice = 10;
       };
       gpu = {
         apply_gpu_optimisations = "accept-responsibility";
@@ -22,10 +22,10 @@
 
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = true;  # Adds "Steam (Gamescope)" to display manager
-    remotePlay.openFirewall = true; # Open ports for Steam Remote Play
-    localNetworkGameTransfers.openFirewall = true; # Open ports for local transfers
-    dedicatedServer.openFirewall = false; # Open ports for Source Dedicated Server
+    gamescopeSession.enable = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    dedicatedServer.openFirewall = false;
   };
 
   environment.systemPackages = with pkgs; [

65cf3d0190afc3e2b898ecfa45ce1ae8ee57c28f
benesamaritan <243715333+benesamaritan@users.noreply.github.com>
2026-01-25 23:21:00 +0700

    update engine back to chromium

--- apps/web-apps.nix
+++ apps/web-apps.nix
@@ -65,12 +65,12 @@ let
     (makeWebApp "Github Copilot Chat"     "https://github.com/copilot"      "github.com"           "github-suite" 0 "")
 
     (makeWebApp "Zoom"                    "https://app.zoom.us/wc/join"     "zoom.us"              "zoom"         0 "")
-    (makeWebApp "Discord"                 "https://discord.com"             "discord.com"          "discord"      1 "")
-    (makeWebApp "Canva"                   "https://www.canva.com"           "canva.com"            "canva"        1 "")
+    (makeWebApp "Discord"                 "https://discord.com"             "discord.com"          "discord"      0 "")
+    (makeWebApp "Canva"                   "https://www.canva.com"           "canva.com"            "canva"        0 "")
     (makeWebApp "WhatsApp"                "https://web.whatsapp.com"        "whatsapp.com"         "whatsapp"     0 "")
-    (makeWebApp "Telegram"                "https://web.telegram.org/k"      "web.telegram.org"     "telegram"     1 "")
-    (makeWebApp "Graphite"                "https://editor.graphite.art"     "graphite.art"         "graphite"     1 "")
-    (makeWebApp "Figma"                   "https://www.figma.com"           "figma.com"            "figma"        1 "")
+    (makeWebApp "Telegram"                "https://web.telegram.org/k"      "web.telegram.org"     "telegram"     1 "https://web.telegram.org/k/manifest.json")
+    (makeWebApp "Graphite"                "https://editor.graphite.art"     "graphite.art"         "graphite"     0 "")
+    (makeWebApp "Figma"                   "https://www.figma.com"           "figma.com"            "figma"        0 "")
     (makeWebApp "DevHub Web App" "https://app.devhubapp.com" "devhubapp.com" "github-suite" 0 "")
     (makeWebApp "HTTPie Web UI" "https://httpie.io/app" "httpie.io" "httpie" 0 "")
   ];

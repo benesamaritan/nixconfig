d3e62eace38b8ad37e3e960750176cb6daf994fe
benesamaritan <243715333+benesamaritan@users.noreply.github.com>
2026-01-26 16:37:06 +0700

    Create Services

---
+++ services/users.services.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{	
+  services.kdeconnect = {
+    enable = true;
+    indicator = true;
+  };
+
+  systemd.user.services.keepassxc = {
+    Unit = {
+      Description = "KeePassXC Password Manager";
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.keepassxc}/bin/keepassxc --minimized";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.services.thunderbird = {
+    Unit = {
+      Description = "Thunderbird Email Client";
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.thunderbird}/bin/thunderbird";
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };   
+
+  systemd.user.services.easyeffects = {
+    Unit = {
+      Description = "EasyEffects Audio Processor";
+      After = [ "pipewire-pulse.service" ];
+      PartOf = [ "pipewire-pulse.service" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.easyeffects}/bin/easyeffects --gapplication-service";
+      Restart = "always";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };   
+}

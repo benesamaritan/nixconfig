6cb5b9a05a53dee122169af86434eb3d0df5d81f
benesamaritan <243715333+benesamaritan@users.noreply.github.com>
2026-01-28 11:18:10 +0700

    Update benesamaritan.nix to modular function without shared vars

--- vars/benesamaritan.nix
+++ vars/benesamaritan.nix
@@ -1,31 +1,16 @@
-{ nixpkgs, ... }:
+{ nixpkgs ? null, ... }:
 
-let
-
-	username = "bye";
-	description = "Bayu Saputro";
-	hashPasswd = "$6$KyNQWAqeKj9uX3G6$zoWMV0vtTLO1cEbnUDKnFGSIy.MGPnoCDQ3mmZisqx2qrr.Ywyp3ajoLhni2OQTWZ4kKAVMNcKSuKzIWPmhx7.";
-	system = "x86_64-linux";
-	pkgs = import nixpkgs {
-	  inherit system;
-	  config.allowUnfree = true;
-	};
-	hostname = "sol";
-	shell = "fish";
-	timezone = "Asia/Jakarta";
-	defaultLocale = "en_US.UTF-8";
-	extraLocale = "id_ID.UTF-8";
-	groups = [
-	  "networkmanager"
-	  "wheel"
-	];
-	xkb = {
-	  layout = "us";
-	  variant = "";
-	};
-	git = {
-	  user = "benesamaritan";
-	  email = "243715333+benesamaritan@users.noreply.github.com";
-	};
-
-in { }
+{
+  username = "bye";
+  description = "Bayu Saputro";
+  hashPasswd = "$6$KyNQWAqeKj9uX3G6$zoWMV0vtTLO1cEbnUDKnFGSIy.MGPnoCDQ3mmZisqx2qrr.Ywyp3ajoLhni2OQTWZ4kKAVMNcKSuKzIWPmhx7.";
+  groups = [
+    "networkmanager"
+    "wheel"
+  ];
+  shell = "fish";
+  git = {
+    user = "benesamaritan";
+    email = "243715333+benesamaritan@users.noreply.github.com";
+  };
+}
